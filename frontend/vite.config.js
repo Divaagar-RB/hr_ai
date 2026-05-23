adfccc68f944ddec58e139154529460347b87f78
Divaagar-RB <divaagarrb@gmail.com>
2026-05-23 07:46:49 +0530

    local tunnel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://two-flies-draw.loca.lt/',
+        target: 'https://light-pens-bet.loca.lt/',
         changeOrigin: true,
         secure: false,
         headers: {
@@ -16,7 +16,7 @@ export default defineConfig({
         }
       },
       '/uploads': {
-        target: 'https://two-flies-draw.loca.lt/',
+        target: 'https://light-pens-bet.loca.lt/',
         changeOrigin: true,
         secure: false,
         headers: {
